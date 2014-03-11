ecd378e67d154232c34bab201da39a0f705d3316
Ciro Duran <ciro.duran@gmail.com>
2014-03-11 22:07:54 +0000

    Added OpenCL code ported from Jos Stam fluid dynamics code. Warning: code compiled for first time, not guaranteed that it will run correctly.

---
+++ assets/opencl/fluids.cl
@@ -0,0 +1,234 @@
+/*** FUNCTIONS FOR FLOAT 2 ***/
+
+__kernel void add_source_float2(__global float2* prev,
+                                __global float2* curr,
+                                int data_width,
+                                float dt) {
+  uint global_addr_x, global_addr_y, idx;
+  global_addr_x = get_global_id(0);
+  global_addr_y = get_global_id(1);
+  idx = (global_addr_y)*(data_width)+global_addr_x;
+  curr[idx] += dt*prev[idx];
+
+}
+__kernel void lin_solve_float2(__global float2* prev,
+                               __global float2* curr,
+                               int data_width,
+                               float a,
+                               float c) {
+
+  uint global_addr_x, global_addr_y, idx11, idx01, idx21, idx10, idx12;
+  global_addr_x = get_global_id(0)+1;
+  global_addr_y = get_global_id(1)+1;
+  idx11 = (global_addr_y)*(data_width)+(global_addr_x);
+  idx01 = (global_addr_y)*(data_width)+(global_addr_x-1);
+  idx21 = (global_addr_y)*(data_width)+(global_addr_x+1);
+  idx10 = (global_addr_y-1)*(data_width)+(global_addr_x);
+  idx12 = (global_addr_y+1)*(data_width)+(global_addr_x);
+
+  curr[idx11] = (prev[idx11] + a*(curr[idx01]+curr[idx21]+curr[idx10]+curr[idx12]))/c;
+}
+
+/* Call with one dimension work item, equals to fluid width */
+__kernel void set_bnd_float2(__global float2 *arr,
+                             int data_width) {
+  uint global_addr_x, global_addr_y, 
+       idx0I, idxN1I, idxI0, idxIN1,
+       idx1I, idxNI,  idxI1, idxIN;
+  global_addr_x = get_global_id(0)+1;
+  idx0I = (global_addr_x)*(data_width)+0;
+  idxN1I = (global_addr_x)*(data_width)+data_width-1;
+  idxI0 = 0*(data_width)+(global_addr_x);
+  idxIN1 = (data_width-1)*(data_width)+(global_addr_x);
+
+  idx1I = (global_addr_x)*(data_width)+1;
+  idxNI = (global_addr_x)*(data_width)+(data_width-2);
+  idxI1 = 1*(data_width)+(global_addr_x);
+  idxIN = (data_width-2)*(data_width)+(global_addr_x);
+
+  arr[idx0I] = arr[idx1I]*(float2)(-1,1);
+  arr[idxN1I] = arr[idxNI]*(float2)(-1,1);
+  arr[idxI0] = arr[idxI1]*(float2)(1,-1);
+  arr[idxI0] = arr[idxIN]*(float2)(1,-1);
+}
+
+__kernel void set_bnd_float2_end(__global float2 *arr,
+                                 int data_width) {
+  uint idx00, idx10, idx01, idx0N1, idx1N1, idx0N,
+       idxN10, idxN0, idxN11, idxN1N1, idxNN1, idxN1N;
+
+  idx00   = 0;
+  idx10   = 1;
+  idx01   = data_width;
+  idx0N1  = (data_width - 1) * data_width + 0;
+  idx1N1  = (data_width - 1) * data_width + 1;
+  idx0N   = (data_width - 2) * data_width;
+  idxN10  = data_width - 1;
+  idxN0   = data_width - 2;
+  idxN11  = (data_width * 1) + data_width - 1;
+  idxN1N1 = (data_width - 1) * data_width + (data_width - 1);
+  idxNN1  = (data_width - 1) * data_width + (data_width - 2);
+  idxN1N  = (data_width - 2) * data_width + (data_width - 1);
+
+  arr[idx00]   = 0.5f * (arr[idx10]  + arr[idx01]);
+  arr[idx0N1]  = 0.5f * (arr[idx1N1] + arr[idx0N]);
+  arr[idxN10]  = 0.5f * (arr[idxN0]  + arr[idxN11]);
+  arr[idxN1N1] = 0.5f * (arr[idxNN1] + arr[idxN1N]);
+}
+
+/*** FUNCTIONS FOR FLOAT 1 ***/
+
+__kernel void lin_solve_float(__global float* prev,
+                              __global float* curr,
+                              int data_width,
+                              float a,
+                              float c) {
+
+  uint global_addr_x, global_addr_y, idx11, idx01, idx21, idx10, idx12;
+  global_addr_x = get_global_id(0)+1;
+  global_addr_y = get_global_id(1)+1;
+  idx11 = (global_addr_y)*(data_width)+(global_addr_x);
+  idx01 = (global_addr_y)*(data_width)+(global_addr_x-1);
+  idx21 = (global_addr_y)*(data_width)+(global_addr_x+1);
+  idx10 = (global_addr_y-1)*(data_width)+(global_addr_x);
+  idx12 = (global_addr_y+1)*(data_width)+(global_addr_x);
+  curr[idx11] = (prev[idx11] + a*(curr[idx01]+curr[idx21]+curr[idx10]+curr[idx12]))/c;
+}
+
+/* Call with one dimension work item, equals to fluid width */
+__kernel void set_bnd_float(__global float *arr,
+                            int data_width) {
+  uint global_addr_x, global_addr_y, 
+       idx0I, idxN1I, idxI0, idxIN1,
+       idx1I, idxNI,  idxI1, idxIN;
+  global_addr_x = get_global_id(0)+1;
+  idx0I = (global_addr_x)*(data_width)+0;
+  idxN1I = (global_addr_x)*(data_width)+data_width-1;
+  idxI0 = 0*(data_width)+(global_addr_x);
+  idxIN1 = (data_width-1)*(data_width)+(global_addr_x);
+
+  idx1I = (global_addr_x)*(data_width)+1;
+  idxNI = (global_addr_x)*(data_width)+(data_width-2);
+  idxI1 = 1*(data_width)+(global_addr_x);
+  idxIN = (data_width-2)*(data_width)+(global_addr_x);
+
+  arr[idx0I] = arr[idx1I];
+  arr[idxN1I] = arr[idxNI];
+  arr[idxI0] = arr[idxI1];
+  arr[idxI0] = arr[idxIN];
+}
+
+__kernel void set_bnd_float_end(__global float *arr,
+                                int data_width) {
+  uint idx00, idx10, idx01, idx0N1, idx1N1, idx0N,
+       idxN10, idxN0, idxN11, idxN1N1, idxNN1, idxN1N;
+
+  idx00   = 0;
+  idx10   = 1;
+  idx01   = data_width;
+  idx0N1  = (data_width - 1) * data_width + 0;
+  idx1N1  = (data_width - 1) * data_width + 1;
+  idx0N   = (data_width - 2) * data_width;
+  idxN10  = data_width - 1;
+  idxN0   = data_width - 2;
+  idxN11  = (data_width * 1) + data_width - 1;
+  idxN1N1 = (data_width - 1) * data_width + (data_width - 1);
+  idxNN1  = (data_width - 1) * data_width + (data_width - 2);
+  idxN1N  = (data_width - 2) * data_width + (data_width - 1);
+
+  arr[idx00]   = 0.5f * (arr[idx10] + arr[idx01]);
+  arr[idx0N1]  = 0.5f * (arr[idx1N1] + arr[idx0N]);
+  arr[idxN10]  = 0.5f * (arr[idxN0] + arr[idxN11]);
+  arr[idxN1N1] = 0.5f * (arr[idxNN1] + arr[idxN1N]);
+}
+
+/** PROJECT **/
+__kernel void project_start(__global float2 *uv,
+                            __global float *p0,
+                            __global float *p1,
+                            int data_width) {
+  uint global_addr_x, global_addr_y, idx11, idx01, idx21, idx10, idx12;
+  global_addr_x = get_global_id(0)+1;
+  global_addr_y = get_global_id(1)+1;
+  idx11 = (global_addr_y)*(data_width)+(global_addr_x);
+  idx01 = (global_addr_y)*(data_width)+(global_addr_x-1);
+  idx21 = (global_addr_y)*(data_width)+(global_addr_x+1);
+  idx10 = (global_addr_y-1)*(data_width)+(global_addr_x);
+  idx12 = (global_addr_y+1)*(data_width)+(global_addr_x);
+
+  p0[idx11] = -0.5f*(uv[idx21].x-uv[idx01].x+uv[idx12].y-uv[idx10].y)/(data_width-2);
+  p1[idx11] = 0;
+}
+
+__kernel void project_end(__global float2 *uv,
+                          __global float *p,
+                          int data_width) {
+  uint global_addr_x, global_addr_y, idx11, idx01, idx21, idx10, idx12;
+  global_addr_x = get_global_id(0)+1;
+  global_addr_y = get_global_id(1)+1;
+  idx11 = (global_addr_y)*(data_width)+(global_addr_x);
+  idx01 = (global_addr_y)*(data_width)+(global_addr_x-1);
+  idx21 = (global_addr_y)*(data_width)+(global_addr_x+1);
+  idx10 = (global_addr_y-1)*(data_width)+(global_addr_x);
+  idx12 = (global_addr_y+1)*(data_width)+(global_addr_x);
+
+  uv[idx11] -= (float2)(0.5f*(data_width-2)*(p[idx21]-p[idx01]),
+                        0.5f*(data_width-2)*(p[idx12]-p[idx10]));
+}
+
+/** ADVECT **/
+__kernel void advect_float2(__global float2 *uv0,
+                            __global float2 *uv1,
+							__global float2 *uv,
+                            int data_width,
+                            float dt0) {
+  uint global_addr_x, global_addr_y, idx, idx00, idx01, idx10, idx11;
+  float2 p, pf0, pf1;
+  int2 pi0, pi1;
+  global_addr_x = get_global_id(0)+1;
+  global_addr_y = get_global_id(1)+1;
+  idx = (global_addr_y)*(data_width)+(global_addr_x);
+  p = clamp((float2)(global_addr_x - dt0*uv[idx].x, global_addr_y - dt0*uv[idx].y),
+            (0.5f, 0.5f),
+            ((data_width-2)+0.5f, (data_width-2)+0.5f);
+  pi0 = convert_int2(p);
+  pi1 = pi0 + (1,1);
+  pf1 = p-pi0;
+  pf0 = 1-pf1;
+
+  idx00 = pi0.y*data_width_+pi0.x;
+  idx01 = pi1.y*data_width_+pi0.x;
+  idx11 = pi1.y*data_width_+pi1.x;
+  idx10 = pi0.y*data_width_+pi1.x;
+
+  uv1[idx] = pf0.x * (pf0.y * uv0[idx00] + pf1.y * uv0[idx01]) +
+             pf1.x * (pf0.y * uv0[idx10] + pf1.y * uv0[idx11]);
+}
+
+__kernel void advect_float(__global float *arr0,
+                           __global float *arr1,
+						   __global float2 *uv,
+                           int data_width,
+                           float dt0) {
+  uint global_addr_x, global_addr_y, idx, idx00, idx01, idx10, idx11;
+  float2 p, pf0, pf1;
+  int2 pi0, pi1;
+  global_addr_x = get_global_id(0)+1;
+  global_addr_y = get_global_id(1)+1;
+  idx = (global_addr_y)*(data_width)+(global_addr_x);
+  p = clamp((float2)(global_addr_x - dt0*uv[idx].x, global_addr_y - dt0*uv[idx].y),
+            (0.5f, 0.5f),
+            ((data_width-2)+0.5f, (data_width-2)+0.5f);
+  pi0 = convert_int2(p);
+  pi1 = pi0 + (1,1);
+  pf1 = p-pi0;
+  pf0 = 1-pf1;
+
+  idx00 = pi0.y*data_width_+pi0.x;
+  idx01 = pi1.y*data_width_+pi0.x;
+  idx11 = pi1.y*data_width_+pi1.x;
+  idx10 = pi0.y*data_width_+pi1.x;
+
+  arr1[idx] = pf0.x * (pf0.y * arr0[idx00] + pf1.y * arr0[idx01]) +
+              pf1.x * (pf0.y * arr0[idx10] + pf1.y * arr0[idx11]);
+}
--- src/examples/layer2/engine.h
+++ src/examples/layer2/engine.h
@@ -13,13 +13,18 @@
 // Copied OpenCL initialisation code from
 // http://www.drdobbs.com/parallel/a-gentle-introduction-to-opencl/231002854?pgno=3
 
+#define IX(i,j) ((i)+(N+2)*(j))
+#define SWAP(x0,x) {float * tmp=x0;x0=x;x=tmp;}
+#define FOR_EACH_CELL for ( i=1 ; i<=N ; i++ ) { for ( j=1 ; j<=N ; j++ ) {
+#define END_FOR }}
+
 namespace octet {
   class engine : public app {
     typedef mat4t mat4t;
     typedef vec4 vec4;
 
     // shaders to draw triangles
-    bump_shader object_shader;
+    texture_shader tshader;
 
     // helper for drawing text
     //text_overlay overlay;
@@ -28,8 +33,46 @@ namespace octet {
     cl_device_id clDeviceID;
     cl_context clContext;
     cl_program clProgram;
-    cl_kernel clKernel;
+    cl_kernel clAddSourceFloat2Kernel;
+    cl_kernel clLinSolveFloat2Kernel;
+    cl_kernel clSetBoundFloat2Kernel;
+    cl_kernel clSetBoundEndFloat2Kernel;
+    cl_kernel clAddSourceFloatKernel;
+    cl_kernel clLinSolveFloatKernel;
+    cl_kernel clSetBoundFloatKernel;
+    cl_kernel clSetBoundEndFloatKernel;
+    cl_kernel clProjectStartKernel;
+    cl_kernel clProjectEndKernel;
+    cl_kernel clAdvectFloat2Kernel;
+    cl_kernel clAdvectFloatKernel;
+
     cl_command_queue clQueue;
+
+    cl_mem uv0_buffer;
+    cl_mem uv1_buffer;
+    cl_mem dens0_buffer;
+    cl_mem dens1_buffer;
+
+    // Fluid data
+    unsigned int N;
+    unsigned int Nborder;
+    float dt;
+    float diff;
+    float visc;
+    float force;
+    float source;
+
+    dynarray <float>uv0;
+    dynarray <float>uv1;
+    dynarray <float>dens0;
+    dynarray <float>dens1;
+
+    float *u;
+    float *u0;
+    float *v;
+    float *v0;
+
+    /*** OPENCL SPECIFIC FUNCTIONS ***/
 
     cl_device_id createDevice() {
       cl_platform_id platform;
@@ -54,6 +97,30 @@ namespace octet {
       }
 
       return dev;
+    }
+
+    cl_kernel createKernel(cl_program prg, const char *kernel_name) {
+      cl_int err;
+      cl_kernel k = clCreateKernel(clProgram, "add_source_float2", &err);
+      if (err < 0) {
+        printf("Could not create kernel %s", kernel_name);
+        return NULL; 
+      }
+      return k;
+    }
+
+    void writeArray(cl_mem dst, float *src, unsigned int size) {
+      cl_int err = clEnqueueWriteBuffer(clQueue, dst, CL_TRUE, 0, size*sizeof(float), (void *)src, 0, NULL, NULL);
+      if (err < 0) {
+        printf("Could not write array.");
+      }
+    }
+
+    void readArray(cl_mem src, float *dst, unsigned int size) {
+      cl_int err = clEnqueueReadBuffer(clQueue, src, CL_TRUE, 0, size*sizeof(float), (void *)dst, 0, NULL, NULL);
+      if (err < 0) {
+        printf("Could not read array.");
+      }
     }
 
     cl_program buildProgram(cl_context ctx, cl_device_id dev, const char *filename) {
@@ -104,29 +171,10 @@ namespace octet {
       return program; 
     }
 
-  public:
-    // this is called when we construct the class
-    engine(int argc, char **argv) : app(argc, argv) {
-    }
-
-    // this is called once OpenGL is initialized
-    void app_init() {
-      // set up the shaders
-      object_shader.init(false);
-
+    void initOpenCL() {
       cl_int err;
-      size_t local_size, global_size;
+      int size = (Nborder)*(Nborder);
 
-      float data[64];
-      float sum[2], total, actual_sum;
-      cl_mem input_buffer, sum_buffer;
-      cl_int num_groups;
-
-      for (int i = 0; i < 64; i ++) {
-        data[i] = 1.0f*i;
-      }
-      
-      // Create device and context
       clDeviceID = createDevice();
       clContext = clCreateContext(NULL, 1, &clDeviceID, NULL, NULL, &err);
       if (err < 0) {
@@ -135,16 +183,17 @@ namespace octet {
       }
 
       // Build program
-      clProgram = buildProgram(clContext, clDeviceID, "assets/add_numbers.cl");
+      clProgram = buildProgram(clContext, clDeviceID, "assets/opencl/fluids.cl");
 
       // Create data buffer
-      global_size = 8;
-      local_size = 4;
-      num_groups = global_size/local_size;
-      input_buffer = clCreateBuffer(clContext, CL_MEM_READ_ONLY |
-        CL_MEM_COPY_HOST_PTR, 64 * sizeof(float), data, &err);
-      sum_buffer = clCreateBuffer(clContext, CL_MEM_READ_WRITE |
-        CL_MEM_COPY_HOST_PTR, num_groups * sizeof(float), sum, &err);
+      uv0_buffer = clCreateBuffer(clContext, CL_MEM_READ_WRITE |
+        CL_MEM_COPY_HOST_PTR, size * 2 * sizeof(float), uv0.data(), &err);
+      dens0_buffer = clCreateBuffer(clContext, CL_MEM_READ_WRITE |
+        CL_MEM_COPY_HOST_PTR, size * sizeof(float), dens0.data(), &err);
+      uv1_buffer = clCreateBuffer(clContext, CL_MEM_READ_WRITE |
+        CL_MEM_COPY_HOST_PTR, size * 2 * sizeof(float), uv1.data(), &err);
+      dens1_buffer = clCreateBuffer(clContext, CL_MEM_READ_WRITE |
+        CL_MEM_COPY_HOST_PTR, size * sizeof(float), dens1.data(), &err);
       if (err < 0) {
         perror("Could not create a buffer");
         return; //exit(1);
@@ -158,63 +207,326 @@ namespace octet {
       }
 
       // Create a Kernel
-      clKernel = clCreateKernel(clProgram, "add_numbers", &err);
+      clAddSourceFloat2Kernel = createKernel(clProgram, "add_source_float2");
+      clLinSolveFloat2Kernel = createKernel(clProgram, "lin_solve_float2");
+      clSetBoundFloat2Kernel = createKernel(clProgram, "set_bnd_float2");
+      clSetBoundEndFloat2Kernel = createKernel(clProgram, "set_bnd_float2_end");
+      
+      clAddSourceFloatKernel = createKernel(clProgram, "add_source_float");
+      clLinSolveFloatKernel = createKernel(clProgram, "lin_solve_float");
+      clSetBoundFloatKernel = createKernel(clProgram, "set_bnd_float");
+      clSetBoundEndFloatKernel = createKernel(clProgram, "set_bnd_float_end");
+
+      clProjectStartKernel = createKernel(clProgram, "project_start");
+      clProjectEndKernel = createKernel(clProgram, "project_end");
+
+      clAdvectFloat2Kernel = createKernel(clProgram, "advect_float2");
+      clAdvectFloatKernel = createKernel(clProgram, "advect_float");
+    }
+    
+    /*** FLUID DYNAMICS FUNCTIONS ***/
+    
+    void dens_step ( int N, float * x, float * x0, float * uv, float diff, float dt )
+    {
+      writeArray(dens0_buffer, x0, (N+2)*(N+2)*sizeof(float));
+      writeArray(dens1_buffer, x, (N+2)*(N+2)*sizeof(float));
+      writeArray(uv0_buffer, uv, (N+2)*(N+2)*2*sizeof(float));
+      add_source(N, dens1_buffer, dens0_buffer, dt, clAddSourceFloatKernel);
+      diffuse(N, dens0_buffer, dens1_buffer, diff, dt, clLinSolveFloatKernel, clSetBoundFloatKernel, clSetBoundEndFloatKernel);
+      advect(N, dens1_buffer, dens0_buffer, uv0_buffer, dt, clAdvectFloatKernel, clSetBoundFloatKernel, clSetBoundEndFloatKernel);
+      readArray(dens0_buffer, x0, (N+2)*(N+2)*sizeof(float));
+      readArray(dens1_buffer, x, (N+2)*(N+2)*sizeof(float));
+    }
+
+    void vel_step ( int N, float * uv, float * uv0, float visc, float dt )
+    {
+      writeArray(uv0_buffer, uv0, (N+2)*(N+2)*2*sizeof(float));
+      writeArray(uv1_buffer, uv, (N+2)*(N+2)*2*sizeof(float));
+      add_source(N, uv1_buffer, uv0_buffer, dt, clAddSourceFloat2Kernel);
+      diffuse(N, uv0_buffer, uv1_buffer, visc, dt, clLinSolveFloat2Kernel, clSetBoundFloat2Kernel, clSetBoundEndFloat2Kernel);
+      project (N, uv0_buffer, dens0_buffer, dens1_buffer);
+      advect(N, uv1_buffer, uv0_buffer, uv0_buffer, dt, clAdvectFloat2Kernel, clSetBoundFloat2Kernel, clSetBoundEndFloat2Kernel);
+      project(N, uv1_buffer, dens0_buffer, dens1_buffer);
+      readArray(uv0_buffer, uv0, (N+2)*(N+2)*2*sizeof(float));
+      readArray(uv1_buffer, uv, (N+2)*(N+2)*2*sizeof(float));
+    }
+
+    void add_source ( int N, cl_mem x, cl_mem s, float dt, cl_kernel clAddSourceKern)
+    {
+      cl_int err;
+
+      size_t global_size[2] = {N, N};
+      size_t local_size[2] = {1, 1};
+      cl_int num_groups[2] = {global_size[0]/local_size[0], global_size[1]/local_size[1]};
+
+      int size = (N+2)*(N+2);
+
+      // Create Kernel Arguments
+      err = clSetKernelArg(clAddSourceKern, 0, sizeof(cl_mem), &s);
+      err |= clSetKernelArg(clAddSourceKern, 1, sizeof(cl_mem), &x);
+      err |= clSetKernelArg(clAddSourceKern, 2, sizeof(cl_mem), &size);
+      err |= clSetKernelArg(clAddSourceKern, 3, sizeof(cl_mem), &dt);
       if (err < 0) {
-        perror("Could not create a kernel");
+        perror("Could not create a kernel argument");
+      }
+
+      // Enqueue kernel
+
+      err = clEnqueueNDRangeKernel(clQueue, clAddSourceKern, 2, NULL, global_size, local_size, 0, NULL, NULL);
+      if (err < 0) {
+        perror("Could not enqueue the kernel");
+      }
+    }
+
+    void set_bnd ( int N, cl_mem x, cl_kernel setBndKern, cl_kernel setBndEndKern)
+    {
+      cl_int err;
+
+      size_t global_size = N;
+      size_t local_size = 1;
+      size_t end_size = 1;
+
+      int Nborder = N + 2;
+
+      err = clSetKernelArg(setBndKern, 0, sizeof(cl_mem), &x);
+      err |= clSetKernelArg(setBndKern, 1, sizeof(cl_mem), &Nborder);
+      if (err < 0) {
+        perror("Could not create a kernel argument for setBndKern");
         return; //exit(1);
       }
 
-      // Create Kernel Arguments
-      err = clSetKernelArg(clKernel, 0, sizeof(cl_mem), &input_buffer);
-      err |= clSetKernelArg(clKernel, 1, local_size * sizeof(float), NULL);
-      err |= clSetKernelArg(clKernel, 2, sizeof(cl_mem), &sum_buffer);
+      err = clSetKernelArg(setBndEndKern, 0, sizeof(cl_mem), &x);
+      err |= clSetKernelArg(setBndEndKern, 1, sizeof(cl_mem), &Nborder);
       if (err < 0) {
-        perror("Could not create a kernel argument");
+        perror("Could not create a kernel argument for setBndEndKern");
+        return; //exit(1);
+      }
+
+      err |= clEnqueueNDRangeKernel(clQueue, setBndKern, 1, NULL, &global_size,
+        &local_size, 0, NULL, NULL);
+      err |= clEnqueueNDRangeKernel(clQueue, setBndEndKern, 1, NULL, &end_size,
+        &end_size, 0, NULL, NULL);
+      if (err < 0) {
+        perror("Could not enqueue the kernel for set_bnd");
+        return; //exit(1);
+      }
+    } 
+
+    void lin_solve(int width, cl_mem x, cl_mem x0, float a, float c,
+                   cl_kernel linSolveKern, cl_kernel setBndKern, cl_kernel setBndEndKern)
+    {
+      cl_int err;
+      int k = 0;
+
+      size_t global_size[2] = {width, width};
+      size_t local_size[2] = {1, 1};
+      cl_int num_groups[2] = {global_size[0]/local_size[0], global_size[1]/local_size[1]};
+      
+      int Nborder = width+2;
+
+      // Create Kernel Arguments
+      err = clSetKernelArg(linSolveKern, 0, sizeof(cl_mem), &x0);
+      err |= clSetKernelArg(linSolveKern, 1, sizeof(cl_mem), &x);
+      err |= clSetKernelArg(linSolveKern, 2, sizeof(cl_mem), &Nborder);
+      err |= clSetKernelArg(linSolveKern, 3, sizeof(cl_mem), &a);
+      err |= clSetKernelArg(linSolveKern, 4, sizeof(cl_mem), &c);
+      if (err < 0) {
+        perror("Could not create a kernel argument for linSolveKern");
         return; //exit(1);
       }
 
       // Enqueue kernel
-      err = clEnqueueNDRangeKernel(clQueue, clKernel, 1, NULL, &global_size,
-        &local_size, 0, NULL, NULL);
+      for (k = 0; k != 20; k++) {
+        err = clEnqueueNDRangeKernel(clQueue, linSolveKern, 2, NULL, global_size,
+          local_size, 0, NULL, NULL);
+        if (err < 0) {
+          perror("Could not enqueue the kernel for linSolveKern");
+          return; //exit(1);
+        }
+        set_bnd( width, x, setBndKern, setBndEndKern );
+      }
+    }
+
+    void diffuse ( int N, cl_mem x, cl_mem x0, float diff, float dt, cl_kernel linSolveKern, cl_kernel setBndKern, cl_kernel setBndEndKern)
+    {
+      float a=dt*diff*N*N;
+      lin_solve ( N, x, x0, a, 1+4*a, linSolveKern, setBndKern, setBndEndKern);
+    }
+    
+    void advect ( int N, cl_mem d, cl_mem d0, cl_mem uv, float dt, cl_kernel advectKern, cl_kernel setBndKern, cl_kernel setBndEndKern )
+    {
+      cl_int err;
+      float dt0 = dt*N;
+      int Nborder = N+2;
+
+      size_t global_size[2] = {N, N};
+      size_t local_size[2] = {1, 1};
+      size_t end_size = 1;
+      cl_int num_groups[2] = {global_size[0]/local_size[0], global_size[1]/local_size[1]};
+
+      // Create Kernel Arguments
+      err = clSetKernelArg(advectKern, 0, sizeof(cl_mem), &d0);
+      err |= clSetKernelArg(advectKern, 1, sizeof(cl_mem), &d);
+      err |= clSetKernelArg(advectKern, 2, sizeof(cl_mem), &uv);
+      err |= clSetKernelArg(advectKern, 3, sizeof(cl_mem), &Nborder);
+      err |= clSetKernelArg(advectKern, 4, sizeof(cl_mem), &dt0);
       if (err < 0) {
-        perror("Could not enqueue the kernel");
+        perror("Could not create a kernel argument for advectKern");
         return; //exit(1);
       }
 
-      // Read Kernel's output
-      err = clEnqueueReadBuffer(clQueue, sum_buffer, CL_TRUE, 0,
-        sizeof(sum), sum, 0, NULL, NULL);
+      err = clEnqueueNDRangeKernel(clQueue, advectKern, 2, NULL, global_size,
+        local_size, 0, NULL, NULL);
       if (err < 0) {
-        perror("Could not read the buffer");
+        perror("Could not enqueue the kernel for advectKern");
         return; //exit(1);
       }
 
-      // Check result
-      total = 0.0f;
-      for (int j = 0; j < num_groups; j++) {
-        total += sum[j];
-      }
-      actual_sum = 1.0f * 64/2*(64-1);
-      printf("Computed sum = %.2f.\n", total);
-      if (fabs(total - actual_sum) > 0.01*fabs(actual_sum))
-        printf("Check failed.\n");
-      else
-        printf("Check passed.\n");
+      set_bnd ( N, d, setBndKern, setBndEndKern );
+    }
 
+    void project ( int N, cl_mem uv, cl_mem p, cl_mem div )
+    {
+      cl_int err;
+      int k = 0;
+
+      size_t global_size[2] = {N, N};
+      size_t local_size[2] = {1, 1};
+      size_t end_size = 1;
+      cl_int num_groups[2] = {global_size[0]/local_size[0], global_size[1]/local_size[1]};
+      
+      int Nborder = N+2;
+      float a = 1.0f;
+      float c = 4.0f;
+      
+      // Create Kernel Arguments
+      err = clSetKernelArg(clProjectStartKernel, 0, sizeof(cl_mem), &uv);
+      err |= clSetKernelArg(clProjectStartKernel, 1, sizeof(cl_mem), &p);
+      err |= clSetKernelArg(clProjectStartKernel, 2, sizeof(cl_mem), &div);
+      err |= clSetKernelArg(clProjectStartKernel, 3, sizeof(cl_mem), &Nborder);
+      if (err < 0) {
+        perror("Could not create a kernel argument for clProjectStartKernel");
+        return; //exit(1);
+      }
+
+      err = clSetKernelArg(clProjectEndKernel, 0, sizeof(cl_mem), &uv);
+      err |= clSetKernelArg(clProjectEndKernel, 1, sizeof(cl_mem), &p);
+      err |= clSetKernelArg(clProjectEndKernel, 2, sizeof(cl_mem), &Nborder);
+      if (err < 0) {
+        perror("Could not create a kernel argument for clProjectEndKernel");
+        return; //exit(1);
+      }
+
+      err = clEnqueueNDRangeKernel(clQueue, clProjectStartKernel, 2, NULL, global_size,
+          local_size, 0, NULL, NULL);
+      if (err < 0) {
+        perror("Could not enqueue the kernel for clProjectStartKernel");
+        return; //exit(1);
+      }
+
+      set_bnd( N, p, clSetBoundFloatKernel, clSetBoundEndFloatKernel );
+      set_bnd( N, div, clSetBoundFloatKernel, clSetBoundEndFloatKernel );
+
+      lin_solve ( N, p, div, 1.0f, 4.0f, clLinSolveFloatKernel, clSetBoundFloatKernel, clSetBoundEndFloatKernel);
+
+      err = clEnqueueNDRangeKernel(clQueue, clProjectEndKernel, 2, NULL, global_size,
+          local_size, 0, NULL, NULL);
+      if (err < 0) {
+        perror("Could not enqueue the kernel for clProjectEndKernel");
+        return; //exit(1);
+      }
+
+      set_bnd( N, uv, clSetBoundFloat2Kernel, clSetBoundEndFloat2Kernel );
+    }
+
+    /*** DEBUG FUNCTIONS ***/
+
+    void print_float(float *arr, int height, int width, int num_comp) {
+      printf("Printing array: [");
+      int j = 0;
+      int size = height*width*num_comp;
+      for (int i = 0; i != size; i++) {
+        if (!(i%width)) printf("\n");
+        if (num_comp > 1) {
+          if (j == 0) printf("(");
+        }
+        printf("%g, ", arr[i]);
+        if (num_comp > 1) {
+          if (j == num_comp-1) printf("), ");
+          j = (j+1)%num_comp;
+        }
+      }
+      printf("]\n");
+    }
+
+  public:
+    // this is called when we construct the class
+    engine(int argc, char **argv)
+    : app(argc, argv)
+    {
+    }
+
+    ~engine() {
       // Deallocate resource
-      clReleaseKernel(clKernel);
-      clReleaseMemObject(sum_buffer);
-      clReleaseMemObject(input_buffer);
+      clReleaseKernel(clLinSolveFloat2Kernel);
+      clReleaseMemObject(uv0_buffer);
+      clReleaseMemObject(uv1_buffer);
+      clReleaseMemObject(dens0_buffer);
+      clReleaseMemObject(dens1_buffer);
       clReleaseCommandQueue(clQueue);
       clReleaseProgram(clProgram);
       clReleaseContext(clContext);
+    }
 
+    // this is called once OpenGL is initialized
+    void app_init() {
+      // set up the shaders
+      tshader.init();
+
+      N = 16;
+      Nborder = N+2;
+      dt = 0.1f;
+      diff = 0.0f;
+      visc = 0.0f;
+      force = 5.0f;
+      source = 100.0f;
+
+      //float a = dt * visc * N * N;
+      //float c = 1 + 4 * a;
+      int size = (Nborder)*(Nborder);
+
+      uv0.resize(size*2);
+      uv1.resize(size*2);
+      dens0.resize(size);
+      dens1.resize(size);
+
+      for (int i = 0; i != size*2; i++) {
+        uv0[i] = 0;
+        uv1[i] = 0;
+      }
+      
+      for (int i = 0; i != size; i++) {
+        dens0[i] = 0;
+        dens1[i] = 0;
+      }
+
+      u = (float *)malloc(size*sizeof(float));
+      u0 = (float *)malloc(size*sizeof(float));
+      v = (float *)malloc(size*sizeof(float));
+      v0 = (float *)malloc(size*sizeof(float));
+
+      // Create device and context
+      initOpenCL();
+      
       //overlay.init();
     }
 
     // this is called to draw the world
     void draw_world(int x, int y, int w, int h) {
-
+      
+      //print_float(uv1.data(), Nborder, Nborder, 2);
+      //print_float(dens1.data(), Nborder, Nborder, 1);
+      
       //int vx = 0, vy = 0;
       //get_viewport_size(vx, vy);
       //overlay.render(object_shader, skin_shader, vx, vy, get_frame_number());
